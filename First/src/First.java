57d65b42897b1b585d68286625e010be7f0bd0c0
51nk0r5w1m <carley4469@gmail.com>
2025-01-13 22:52:14 -0500

    Added First.class after overriding .gitignore

---
+++ First/src/First.java
@@ -0,0 +1,5 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo!\nJob title: Information Security Analyst\nWhy: Never a dull moment\nMedian salary: $120,360");
+    }
+}
