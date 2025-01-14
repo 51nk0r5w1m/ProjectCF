f10d01acbfe99f0af7426cdbd9821beecd0ba925
51nk0r5w1m <111602071+51nk0r5w1m@users.noreply.github.com>
2025-01-13 22:22:39 -0500

    Delete First/src directory

--- First/src/First.java
+++
@@ -1,7 +0,0 @@
-//First.java
-//Carley Fant
-public class First {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo!\nJob title: Information Security Analyst\nWhy: Never a dull moment\nMedian salary: $120,360");
-    }
-}
