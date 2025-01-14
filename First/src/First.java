b2241db069b7990b2f1b2e871eb50a4ad54441fd
51nk0r5w1m <carley4469@gmail.com>
2025-01-13 21:39:07 -0500

    Add First.java and First.class

--- First/src/First.java
+++ First/src/First.java
@@ -1,3 +1,5 @@
+//First.java
+//Carley Fant
 public class First {
     public static void main(String[] args) {
         System.out.println("Hola mundo!\nJob title: Information Security Analyst\nWhy: Never a dull moment\nMedian salary: $120,360");
