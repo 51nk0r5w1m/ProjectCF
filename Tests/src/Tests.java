458f6e72e0032b4f96633eef75e40778295c05e7
51nk0r5w1m <carley4469@gmail.com>
2025-01-24 15:14:45 -0500

    "Added Tests.java"

---
+++ Tests/src/Tests.java
@@ -0,0 +1,16 @@
+public class Tests {
+    public static void main(String[] args) {
+
+        float test1 = 88.2f;
+        float test2 = 78.9f;
+        float test3 = 97.6f;
+
+        float average = (test1 + test2 + test3) / 3;
+        float roundedAverage = Math.round(average*100)/100f;
+        System.out.println("Test score 1: " + test1);
+        System.out.println("Test score 2: " + test2);
+        System.out.println("Test score 3: " + test3);
+        System.out.println("The average of 3 tests scores is: " + roundedAverage);
+
+    }
+}
